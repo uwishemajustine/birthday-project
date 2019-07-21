e1714572f0e8e3345800ad5fe388f4415fc03b6f
uwishemajustine/alsen2 <alsenjastine90@gmail.com>
2019-07-21 20:00:53 +0200

    putting the console.log in the function

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ function numbers(){
     var males=[Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi,  Kwame];
     
     var females=[Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama];
-    
+    var dd=parseInt(document.getElementById["day"]).value;
+var mm=parseInt(document.getElementById["month"]).value;
+var yy=parseInt(document.getElementById["year"]).value;
+var cc=((yy-1)/100+1);
+var result= ((((cc/4) -2*cc-1) +((5*yy/4)) + ((26*(mm+1)/10)) +dd) %7);
+console.log(dd/mm/yy);
+document.write("result").getElementById.HTML=Math.round(result);
+}
