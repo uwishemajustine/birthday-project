fc6f388a458acc537a611be8444ee8777bfc4433
uwishemajustine/alsen2 <alsenjastine90@gmail.com>
2019-07-21 19:58:36 +0200

    adding the variables in javascript

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+function numbers(){
+    var days=[Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
 
+    var months=[January, February, March, April, May, June, July, August, September, October, November, December];
+    var males=[Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi,  Kwame];
+    
+    var females=[Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama];
     
