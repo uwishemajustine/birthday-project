a51f85df7d5d58bba7f6f26ee5b2e8dfa79ab095
uwishemajustine/alsen2 <alsenjastine90@gmail.com>
2019-07-22 19:02:20 +0200

    changes in html, css, javascript

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function birthday(){
-    
+
     var dd=parseInt(document.getElementById("day").value);
+    console.log(dd);
     var mm=parseInt(document.getElementById("month").value);
+    console.log(mm);
     var yy=parseInt(document.getElementById("year").value);
+    console.log(yy);
+    
     var cc=parseInt((yy-1)/100+1);
     var result=parseInt((((cc/4) -2*cc-1) +((5*yy/4)) + ((26*(mm+1)/10)) + dd) %7);
 
 alert(Math.round(result));
-var result=document.querySelector("input [name='gender']:checked").value;
-alert(gg);
-if(dd==0 && gg=="Female") {
+var gg=document.querySelector('input[name="gender"]:checked').value;
+if(mm<1 && mm >=12);
+if(yy<1990 && yy>=2019) {
+alert(result);
+if(dd==0 && gg=="Female") 
     alert("Your birthday is in sunday and your name is Akosua");
 }else if(dd==1 && gg=="Female"){
     alert("Your birthday is in monday and your name is Adwoa");
@@ -43,7 +49,7 @@ if(dd==0 && gg=="Male") {
     alert("Your birthday is in saturday and your name is Kwame");  
 }
 else{
-    alert("waaw");
+    alert("woow");
 }
 }
 
